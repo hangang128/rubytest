bc175aeb62286608be8d3d38e7e65089e943faea
Paul Han <hangang128@gmail.com>
2012-02-21 02:15:07 -0800

    final commit

--- app/controllers/say_controller.rb
+++ app/controllers/say_controller.rb
@@ -1,6 +1,14 @@
 class SayController < ApplicationController
   def hello
     @time=Time.now
+    
+    @files = Dir.glob('*')
+
+#    <% for file in @files %>
+#    file name is: <%= file %>
+#    <% end %>
+
+    
   end
 
   def goodbye
